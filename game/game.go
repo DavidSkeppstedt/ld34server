7e4e70c5a92283e22662e1dac4dfac53a84bea5c
David Skeppstedt <david.skeppstedt@gmail.com>
2015-12-12 21:17:58 +0100

    Making go happy so that we can build

--- game/game.go
+++ game/game.go
@@ -1,14 +1,18 @@
 package game
 
 import (
-	"time"
+	"github.com/DavidSkeppstedt/ld34server/game/player"
+	_ "time"
 )
+
+var Pmanager player.PlayerManager
 
 func StartGame() {
 	go func() {
-		timer := time.Tick(5 * Time.Millisecond)
+		/*timer := time.Tick(5 * time.Millisecond)
 		for now := range timer {
 			//Update game logic here..
-		}
+			log.Println(now)
+		}*/
 	}()
 }
