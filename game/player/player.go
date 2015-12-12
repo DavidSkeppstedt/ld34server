c7eaf3ee193c8e982fed5c7ebfbdab815d7fe070
David Skeppstedt <david.skeppstedt@gmail.com>
2015-12-12 18:14:16 +0100

    Implementent more functionallity for the player type. Now we can move & change angle.

--- game/player/player.go
+++ game/player/player.go
@@ -1,21 +1,44 @@
 package player
 
+import (
+	"math"
+)
+
 type Player struct {
 	Pos   Position
-	Angle float64
+	Angle float32
 }
 
 type Position struct {
-	X float64
-	Y float64
+	X float32
+	Y float32
+}
+
+func (this *Player) Update() {
+	this.move(5)
+}
+func (this *Player) AngleInc(amt float32) {
+	this.Angle += amt
+}
+
+func (this *Player) move(speed float32) {
+	this.Pos.X += speed * float32(math.Cos(float64(this.Angle)*math.Pi/180))
+	this.Pos.Y += speed * float32(math.Sin(float64(this.Angle)*math.Pi/180))
 }
 
 type PlayerManager struct {
 	Players []Player
 }
 
+func (this *PlayerManager) Update() {
+	for _, player := range this.Players {
+		player.Update()
+	}
+}
+
 func (this *PlayerManager) CreatePlayer() Player {
-	p := &Player{}
+	pos := &Position{700, 398}
+	p := &Player{Pos: *pos}
 	this.Players = append(this.Players, *p)
 	return *p
 }
