c0c2571d376509aaf13819717e99379255ed1ce5
David Skeppstedt <david.skeppstedt@gmail.com>
2015-12-12 13:11:35 +0100

    A first simple Hello tcp socket server.

---
+++ app.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/DavidSkeppstedt/ld34server/host"
+)
+
+func main() {
+	host.ListenAndServe()
+}
---
+++ host/server.go
@@ -0,0 +1,27 @@
+package host
+
+import (
+	"log"
+	"net"
+)
+
+func ListenAndServe() {
+	log.Println("Initializing the server")
+	socket, err := net.Listen("tcp", ":7978")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println("Initialization complete. Listening...")
+	for {
+		con, err := socket.Accept()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		go handleConnection(con)
+	}
+}
+func handleConnection(con net.Conn) {
+	defer con.Close()
+	con.Write([]byte("Hello"))
+}
