db9755f367797204e912291c0bc8c7f104205f24
David Skeppstedt <david.skeppstedt@gmail.com>
2015-12-14 23:09:55 +0100

    bulletmanager is now updating bullets every game tick!

--- game/game.go
+++ game/game.go
@@ -6,6 +6,7 @@ import (
 )
 
 var Pmanager player.PlayerManager
+var Bmanager player.BulletManager
 
 func StartGame() {
 	go func() {
@@ -13,6 +14,7 @@ func StartGame() {
 		for range timer {
 			//Update game logic here..
 			Pmanager.Update()
+			Bmanager.Update()
 		}
 	}()
 }
