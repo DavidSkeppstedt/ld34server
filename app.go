bcba1e7de6d38875740be076a85ad19d251caf60
David Skeppstedt <david.skeppstedt@gmail.com>
2015-12-14 18:36:48 +0100

    Set up the program to run on as many cores as the processor have. Making use of the extra power of go-routines.

--- app.go
+++ app.go
@@ -3,8 +3,13 @@ package main
 import (
 	"github.com/DavidSkeppstedt/ld34server/game"
 	"github.com/DavidSkeppstedt/ld34server/host"
+	"runtime"
 )
 
+func init() {
+	numcpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(numcpu) // Try to use all available CPUs.
+}
 func main() {
 	game.StartGame()
 	host.ListenAndServe()
